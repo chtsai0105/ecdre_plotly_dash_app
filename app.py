c0ff89e9f751f8d9cc8eba91d7f34bdcc32890c4
Cheng-Hung <chenghung.tsai@email.ucr.edu>
2024-08-21 11:36:23 -0700

    Update script and requirements.txt

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ amplicon_types = ("16S", "ITS")
 phylo_ranks = ("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
 
 metadata_df = pd.read_csv("metadata.csv", index_col=0)
-taxa_dfs = {amplicon_type: pd.read_csv(file, index_col=0) for amplicon_type, file in zip(amplicon_types, ("taxa_16s.csv", "taxa_its.csv"))}
-otu_dfs = {amplicon_type: pd.read_csv(file, index_col=0) for amplicon_type, file in zip(amplicon_types, ("otu_16s.csv", "otu_its.csv"))}
+taxa_dfs = {
+    amplicon_type: pd.read_csv(file, index_col=0) for amplicon_type, file in zip(amplicon_types, ("taxa_16s.csv", "taxa_its.csv"))
+}
+otu_dfs = {
+    amplicon_type: pd.read_csv(file, index_col=0) for amplicon_type, file in zip(amplicon_types, ("otu_16s.csv", "otu_its.csv"))
+}
 treatment_dict_CA = {
     "CCC": "Control",
     "MCC": "Mulch",
@@ -21,6 +25,44 @@ treatment_dict_CA = {
 treatment_dict_app = {v: k for k, v in treatment_dict_CA.items()}
 
 
+app = Dash(__name__)
+server = app.server
+
+app.layout = [
+    html.Div(children="ECDRE year 0 and year 1 comparison"),
+    html.Hr(),
+    html.Label("Amplicon type:"),
+    dcc.RadioItems(options=amplicon_types, value="16S", id="amplicon-type"),
+    html.Div(
+        children=[
+            html.Div(
+                children=[
+                    html.Label("Tissue type:"),
+                    dcc.Dropdown(options=metadata_df["Tissue_type"].unique(), value="Bulk soil", id="tissue", clearable=False),
+                ],
+                style={"padding": 10, "flex": 1},
+            ),
+            html.Div(
+                children=[
+                    html.Label("Treatment:"),
+                    dcc.Dropdown(options=list(treatment_dict_app.keys()), value="Control", id="treatment", clearable=False),
+                ],
+                style={"padding": 10, "flex": 1},
+            ),
+            html.Div(
+                children=[
+                    html.Label("Phylogeny rank:"),
+                    dcc.Dropdown(options=phylo_ranks[1:], value="Phylum", id="rank", clearable=False),
+                ],
+                style={"padding": 10, "flex": 1},
+            ),
+        ],
+        style={"display": "flex", "flexDirection": "row"},
+    ),
+    dcc.Graph(figure={}, id="percentage-stacked-bar"),
+]
+
+
 def get_dataframe(amplicon_type, tissue, treatment, rank):
     otu_df = otu_dfs[amplicon_type]
     taxa_df = taxa_dfs[amplicon_type]
@@ -32,32 +74,6 @@ def get_dataframe(amplicon_type, tissue, treatment, rank):
     perc_df = perc_df.reindex(perc_df.mean(axis=1).sort_values(ascending=False).index)
     perc_df = perc_df[perc_df.sum(axis=1) != 0]
     return perc_df, metadata
-
-
-def create_dash_app():
-    app = Dash()
-    app.layout = [
-        html.Div(children="ECDRE year 0 and year 1 comparison"),
-        html.Hr(),
-        html.Label("Amplicon type:"),
-        dcc.RadioItems(options=amplicon_types, value="16S", id="amplicon-type"),
-        html.Div(children=[
-            html.Div(children=[
-                html.Label("Tissue type:"),
-                dcc.Dropdown(options=metadata_df["Tissue_type"].unique(), value="Bulk soil", id="tissue", clearable=False),
-            ], style={"padding": 10, "flex": 1}),
-            html.Div(children=[
-                html.Label("Treatment:"),
-                dcc.Dropdown(options=list(treatment_dict_app.keys()), value="Control", id="treatment", clearable=False),
-            ], style={"padding": 10, "flex": 1}),
-            html.Div(children=[
-                html.Label("Phylogeny rank:"),
-                dcc.Dropdown(options=phylo_ranks[1:], value="Phylum", id="rank", clearable=False),
-            ], style={"padding": 10, "flex": 1}),
-        ], style={"display": "flex", "flexDirection": "row"}),
-        dcc.Graph(figure={}, id="percentage-stacked-bar"),
-    ]
-    return app
 
 
 # Add controls to build the interaction
@@ -74,21 +90,21 @@ def update_graph(amplicon_type, tissue, treatment, rank):
     for name in perc_df.index:
         fig.add_trace(
             go.Bar(
-                x=[[f"{x}, {y}" 
-                    for x, y in zip(
-                        metadata.loc[perc_df.columns, "Tissue_type"],
-                        metadata.loc[perc_df.columns, "Year"])
-                        ],
-                    perc_df.columns],
+                x=[
+                    [
+                        f"{x}, {y}"
+                        for x, y in zip(metadata.loc[perc_df.columns, "Tissue_type"], metadata.loc[perc_df.columns, "Year"])
+                    ],
+                    perc_df.columns,
+                ],
                 y=perc_df.loc[name],
                 name=name,
-                )
             )
+        )
     fig.update_layout(barmode="stack")
     return fig
 
 
 if __name__ == "__main__":
-    app = create_dash_app()
-    app.run()
-    # app.run_server(host="0.0.0.0", port=8080)
+    # app.run()
+    app.run_server()
